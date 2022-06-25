39c9a84c75fbcb5445e86e01e3f03044159db3b4
kkouji <kimurazaka+blog@gmail.com>
2022-06-25 23:42:13 +0900

    add jsonschema

--- publish.py
+++ publish.py
@@ -1,16 +1,25 @@
-from typing import Union
+from jsonschema import validate, ValidationError
 import sys, os, json
 
 def main() -> None:
     args = sys.argv
 
-    contents = getJsonFromArgs(args)
-    if contents is not None:
-        print(json.dumps(contents))
-        print(type(contents))
+    contents = get_json_from_args(args)
+    if contents is None or not validate_json_schema(contents):
+        sys.exit('json error.')
+
+def validate_json_schema(contents: dict|list) -> bool:
+    with open('schema.json') as schemaObj:
+        schema = json.load(schemaObj)
+        try:
+            validate(contents, schema)
+            return True
+        except ValidationError as e:
+            print('json validate error')
+    return False
 
 # コマンドライン引数からjsonを取得する
-def getJsonFromArgs(args: list) -> dict|list:
+def get_json_from_args(args: list) -> dict|list:
     if 3 == len(args) and args[2] == os.environ.get('DEPLOY_KEY') :
         try:
             js = json.loads(args[1])
