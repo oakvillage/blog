a0c961dc9fe869c926d41ef4294c492655454644
kkouji <kimurazaka+blog@gmail.com>
2022-06-25 22:42:55 +0900

    fix deploy key

--- publish.py
+++ publish.py
@@ -2,7 +2,11 @@ import sys, os
 
 def main():
     contents = args = sys.argv[1]
-    print(contents)
+    deploy_key = sys.argv[2]
+    if deploy_key == os.environ.get('DEPLOY_KEY'):
+        print(contents)
+    else:
+        print('deploy_key invalid.')
 
 # メイン処理
 if __name__ == '__main__':
