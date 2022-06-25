be29090c5f92c22a22e86c9b47c87ae29e205b06
kkouji <kimurazaka+blog@gmail.com>
2022-06-25 23:02:48 +0900

    fix deploy key

--- publish.py
+++ publish.py
@@ -1,12 +1,23 @@
 import sys, os
 
 def main():
-    contents = args = sys.argv[1]
-    deploy_key = sys.argv[2]
-    if deploy_key == os.environ.get('DEPLOY_KEY'):
-        print(contents)
+    args = sys.argv
+
+    contents = getJsonFromArgs(args)
+    if contents is not None:
+        print(json.dumps(contents))
+
+# コマンドライン引数からjsonを取得する
+def getJsonFromArgs(args: list) -> dict:
+    if 3 == len(args) and args[2] == os.environ.get('DEPLOY_KEY') :
+        try:
+            json = json.loads(args[1])
+            return json
+        except json.JSONDecodeError as e:
+            print(e)
     else:
-        print('deploy_key invalid.')
+        print('Argments are not valid.')
+    return None
 
 # メイン処理
 if __name__ == '__main__':
