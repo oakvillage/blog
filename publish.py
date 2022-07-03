344af112e2ef75307b2fa81fc9c8ec814c17700c
kkouji <kimurazaka+blog@gmail.com>
2022-07-03 16:39:57 +0900

    add appException

--- publish.py
+++ publish.py
@@ -1,12 +1,46 @@
 from jsonschema import validate, ValidationError
 import sys, os, json
 
+DIST_DIR = 'docs'
+
+# Githubにてデプロイ中のエラーを検出
+class AppError(Exception): pass
+
 def main() -> None:
     args = sys.argv
-
+    # 引数からjsonを取得、検証を行う
     contents = get_json_from_args(args)
     if contents is None or not validate_json_schema(contents):
         sys.exit('json error.')
+
+    try:
+        # 記事を出力する
+        output_html(contents)
+
+        # 検索用インデックスのjsonを追加し、出力する
+
+        # コメントを受け付ける為のissueを作成する
+
+    except AppError as e:
+        print('')
+        sys.exit('')
+
+    except:
+        sys.exit('')
+
+# コマンドライン引数からjsonを取得する
+def get_json_from_args(args: list) -> dict|list:
+    if 3 == len(args) and args[2] == os.environ.get('DEPLOY_KEY') :
+        try:
+            js = json.loads(args[1])
+            return js
+        except json.JSONDecodeError as e:
+            print(e)
+        except:
+            print('System error.')
+    else:
+        print('Argments are not valid.')
+    return None
 
 # 受け取ったjsonを検証する
 def validate_json_schema(contents: dict|list) -> bool:
@@ -19,19 +53,12 @@ def validate_json_schema(contents: dict|list) -> bool:
             print('json validate error')
     return False
 
-# コマンドライン引数からjsonを取得する
-def get_json_from_args(args: list) -> dict|list:
-    if 3 == len(args) and args[2] == os.environ.get('DEPLOY_KEY') :
-        try:
-            js = json.loads(args[1])
-            return js
-        except json.JSONDecodeError as e:
-            print(e)
-        except:
-            print('error.')
-    else:
-        print('Argments are not valid.')
-    return None
+# 記事内容をHTMLにて出力する
+def output_html(contents: list) -> None:
+    for content in contents:
+        filename = 
+        with open(filename) as js:
+            
 
 # メイン処理
 if __name__ == '__main__':
